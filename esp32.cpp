8ee4c54c02a63625bd754ffd407b84de8e5ec251
arkaddev <105284547+arkaddev@users.noreply.github.com>
2025-01-26 20:27:04 +0100

    wifi connection was added to esp32.cpp

--- esp32.cpp
+++ esp32.cpp
@@ -2,31 +2,62 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
-// Tworzymy obiekt czujnika BMP280
-Adafruit_BMP280 bmp; // Domyślnie używa I2C
+#include <WiFi.h>
+
+Adafruit_BMP280 bmp;
+
+const char* ssid = "";
+const char* password = "";
+
+
+
 
 void setup() {
   // Uruchamiamy komunikację szeregowa
   Serial.begin(115200);
-  
-  // Inicjalizujemy komunikację I2C i czujnik BMP280
-  if (!bmp.begin(0x76)) {
-    Serial.println("Nie udało się znaleźć czujnika BMP280!");
-    while (1); // Zatrzymujemy program, jeśli czujnik nie został znaleziony
-  }
 
-  Serial.println("Czujnik BMP280 połączony!");
+  temperatureSensor();
+  
+  connectToWifi();
+
 }
 
 void loop() {
-  // Odczytujemy temperaturę, ciśnienie i wysokość
+  
   float temperature = bmp.readTemperature();  // Temperatura w °C
  
-  // Wyświetlamy wyniki w monitorze szeregowym
   Serial.print("Temperatura: ");
   Serial.print(temperature);
   Serial.println(" °C");
   
 
-  delay(2000); // Czekamy 2 sekundy przed kolejnym odczytem
+  delay(2000); 
+  
+  }
+
+
+
+void temperatureSensor(){
+
+  if (!bmp.begin(0x76)) {
+    Serial.println("Nie udało się znaleźć czujnika BMP280!");
+    while (1);
+  }
+
+  Serial.println("Czujnik BMP280 połączony!");
+}
+
+void connectToWifi(){
+
+  WiFi.begin(ssid, password);
+
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);  
+    Serial.println("Łączenie z Wi-Fi...");
+  }
+
+  Serial.println("Połączono z Wi-Fi!");
+  Serial.print("Adres IP: ");
+  Serial.println(WiFi.localIP()); 
 }
