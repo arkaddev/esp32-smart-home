27365b4115473ab53b1e3da14a5ade9b14d1142a
arkaddev <105284547+arkaddev@users.noreply.github.com>
2025-02-01 23:44:29 +0100

    oled connection was added to esp32.cpp

--- esp32.cpp
+++ esp32.cpp
@@ -2,6 +2,10 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
 #include <WiFi.h>
 
 #include <HTTPClient.h>
@@ -14,6 +18,16 @@ const char* password = "";
 const char* serverUrl = "/receive.php";
 
 
+
+// Ustawienia dla OLED (128x64 pikseli, I2C)
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+
+// Adres I2C dla Twojego wyświetlacza: 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 void setup() {
   // Uruchamiamy komunikację szeregowa
   Serial.begin(115200);
@@ -21,6 +35,8 @@ void setup() {
   temperatureSensor();
   
   connectToWifi();
+
+  displayOnOled();
 
 }
 
@@ -33,6 +49,18 @@ void loop() {
   Serial.print(temperature);
   Serial.println(" °C");
   
+
+display.clearDisplay();
+display.setTextSize(3);      // Ustawienie większego rozmiaru tekstu (np. 2)
+  display.setTextColor(SSD1306_WHITE);  // Kolor tekstu (biały)
+  display.setCursor(0, 0);     // Ustawienie pozycji kursora
+
+  // Wyświetl tekst na OLED
+  display.println(temperature);
+  
+  // Zaktualizuj ekran
+  display.display();
+
 
 
 
@@ -53,7 +81,7 @@ void loop() {
   http.end();
 
 
-  delay(60000); 
+  delay(5000); 
   
   }
 
@@ -82,4 +110,29 @@ void connectToWifi(){
   Serial.println("Połączono z Wi-Fi!");
   Serial.print("Adres IP: ");
   Serial.println(WiFi.localIP()); 
+}
+
+void displayOnOled(){
+
+// Inicjalizacja wyświetlacza z adresem 0x3C
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Adres 0x3C
+    Serial.println(F("Nie udało się zainicjować wyświetlacza OLED"));
+    for (;;);  // Jeśli nie uda się zainicjować, zatrzymaj program
+  }
+
+  // Wyczyść ekran
+  display.clearDisplay();
+
+  // Ustawienie tekstu na większy rozmiar
+  display.setTextSize(2);      // Ustawienie większego rozmiaru tekstu (np. 2)
+  display.setTextColor(SSD1306_WHITE);  // Kolor tekstu (biały)
+  display.setCursor(0, 0);     // Ustawienie pozycji kursora
+
+  // Wyświetl tekst na OLED
+  display.println(F("Hello, World!"));
+  
+  // Zaktualizuj ekran
+  display.display();
+
+
 }
